d232381be396ef8121160627c6687d8782c4f66f
Pujan <shresthapujan0010@gmail.com>
2025-02-20 22:52:14 +0545

    added admin link

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,9 @@
+import { Link } from "react-router";
+import { getCurrentUser } from "../authService";
 
 const Footer = () => {
+    const bytes = atob(sessionStorage.getItem("adminRole"));
+    const text = new TextDecoder('utf-8').decode(new Uint8Array([...bytes].map(char => char.charCodeAt(0))));
     return (
         <footer className="bg-zinc-800 w-full text-white h-15 flex items-center justify-between px-10 relative bottom-0">
             <div className="flex flex-col items-center">
@@ -13,6 +17,7 @@ const Footer = () => {
                 <li className="cursor-pointer hover:bg-gray-700 p-2">
                     <a href="mailto:omnisalemarket@gmail.com">Contact Us</a>
                 </li>
+                {getCurrentUser() && text == "PujanNirjala" && <li className="cursor-pointer hover:bg-gray-700 p-2"><Link to="/admin">Admin</Link></li>}
             </ul>
             <span>&copy;2025 OmniSale</span>
         </footer>
