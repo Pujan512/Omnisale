84ea9ad9708ce42915f37fcab39424ba120ec070
Pujan <shresthapujan0010@gmail.com>
2025-02-22 12:37:23 +0545

    responsive 2

--- src/components/categories/Category.jsx
+++ src/components/categories/Category.jsx
@@ -8,7 +8,7 @@ const Category = () => {
     return (
         <section id="category">
             <h2 className="text-xl md:text-2xl z-9 font-semibold my-4">Browse By Category</h2>
-            <section className="md:grid md:my-5 gap-2 flex flex-wrap md:grid-cols-6 md:gap-10">
+            <section className="md:grid md:my-5 gap-1 flex flex-wrap md:grid-cols-6 md:gap-10">
                 {categories.map((category, index) => {
                     return <CategoryCard
                         key={index}
--- src/components/categories/CategoryCard.jsx
+++ src/components/categories/CategoryCard.jsx
@@ -8,7 +8,7 @@ const CategoryCard = ({imgSrc, name, id}) => {
         navigate(`/category/${id}`);
     }
     return (
-        <article className="w-22.5 h-25 md:h-50 md:w-40 flex flex-col justify-center bg-zinc-200 p-2 rounded-md" onClick={() => handleClick(id)}>
+        <article className="w-[21.5vw] h-27 md:h-50 md:w-40 flex flex-col justify-center bg-zinc-200 p-2 rounded-md" onClick={() => handleClick(id)}>
             <img src={imgSrc} alt={name} className="h-10 md:h-30 object-contain"/>
             <p className="text-xs md:text-base text-center font-semibold">{name}</p>
         </article>
